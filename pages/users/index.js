b6f766599c51d6270eb257ae2edeba6bfe2c6e2f
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-08-16 18:47:06 +0530

    SSR overview and dynamic route generation

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import Link from 'next/link'
 
-const index = () => {
-    return (
-        <div>
-            <h1>this is user main page</h1>
+const index = ({ users }) => {
+  return (
+    <div>
+      <h1>The total users: {users.length}</h1>
+      {users.map((user) => (
+        <div key={user.id}>
+          <h4>
+            Name : {user.name}           
+            <Link href={`/users/${user.id}`}>
+              <button>explore</button>
+            </Link>
+          </h4>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default index;
+
+// Data Load instead of useEffect
+export async function getStaticProps(context) {
+  const result = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await result.json();
+  return {
+    props: { users: data }, // will be passed to the page component as props
+  };
+}
